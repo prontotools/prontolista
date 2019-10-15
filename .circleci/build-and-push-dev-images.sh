be0d3bc2ecdff9941e42a9a72b876b14fe115409
zkan <kan@prontomarketing.com>
2019-10-15 21:34:30 +0700

    :whale: Add aws ecr login :bear:

--- .circleci/build-and-push-dev-images.sh
+++ .circleci/build-and-push-dev-images.sh
@@ -2,6 +2,7 @@
 
 poetry run aws configure --profile prontolista set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 poetry run aws configure --profile prontolista set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+eval $(poetry run aws ecr get-login --no-include-email --region eu-west-1 --profile prontolista)
 
 docker build -f compose/django/Dockerfile-dev -t 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:${CIRCLE_SHA1} .
 docker push 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:${CIRCLE_SHA1}
