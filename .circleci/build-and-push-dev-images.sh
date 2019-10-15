141228d67e5a1b753b505eb9d5d957706d738272
zkan <kan@prontomarketing.com>
2019-10-15 22:30:07 +0700

    :whale: Shorten Docker image tag :bear:

--- .circleci/build-and-push-dev-images.sh
+++ .circleci/build-and-push-dev-images.sh
@@ -4,5 +4,7 @@ poetry run aws configure --profile prontolista set aws_access_key_id $AWS_ACCESS
 poetry run aws configure --profile prontolista set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 eval $(poetry run aws ecr get-login --no-include-email --region eu-west-1 --profile prontolista)
 
-docker build -f compose/django/Dockerfile-dev -t 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:$CIRCLE_SHA1 .
-docker push 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:$CIRCLE_SHA1
+export GIT_COMMIT_SHORT=$(echo $CIRCLE_SHA1 | cut -c 1-8)
+
+docker build -f compose/django/Dockerfile-dev -t 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:$GIT_COMMIT_SHORT .
+docker push 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:$GIT_COMMIT_SHORT
