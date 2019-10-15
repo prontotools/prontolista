749a70226a884ccf57d771b8817c572110032ac7
zkan <kan@prontomarketing.com>
2019-10-15 21:41:58 +0700

    :pencil2: Remove { } from env variables :bear:

--- .circleci/build-and-push-dev-images.sh
+++ .circleci/build-and-push-dev-images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-poetry run aws configure --profile prontolista set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-poetry run aws configure --profile prontolista set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+poetry run aws configure --profile prontolista set aws_access_key_id $AWS_ACCESS_KEY_ID
+poetry run aws configure --profile prontolista set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 eval $(poetry run aws ecr get-login --no-include-email --region eu-west-1 --profile prontolista)
 
-docker build -f compose/django/Dockerfile-dev -t 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:${CIRCLE_SHA1} .
-docker push 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:${CIRCLE_SHA1}
+docker build -f compose/django/Dockerfile-dev -t 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:$CIRCLE_SHA1 .
+docker push 133506877714.dkr.ecr.eu-west-1.amazonaws.com/prontolista:$CIRCLE_SHA1
