7a52a599cefee107ed11f9dc12824575682a51c9
zkan <kan@prontomarketing.com>
2019-10-15 22:08:25 +0700

    :construction: Test echo env inside double quotes :bear:

--- .circleci/build-and-push-dev-images.sh
+++ .circleci/build-and-push-dev-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo $AWS_ACCESS_KEY_ID
-echo ${AWS_ACCESS_KEY_ID}
+echo "$AWS_ACCESS_KEY_ID"
+echo "${AWS_ACCESS_KEY_ID}"
 
 poetry run aws configure --profile prontolista set aws_access_key_id $AWS_ACCESS_KEY_ID
 poetry run aws configure --profile prontolista set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
