352f098d7b16024e7f589f2db1a8c86b143334ff
zkan <kan@prontomarketing.com>
2019-10-17 06:47:02 +0700

    :rocket: Add Ansible deploy for production :bear:

---
+++ ansible/deploy-production.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export GIT_COMMIT_SHORT=$(echo $CIRCLE_SHA1 | cut -c 1-8)
+echo $VAULT_PASSWORD > vault-password-file
+cp ../api/docker-compose.production.yml ./roles/deploy/files/
+poetry run ansible-playbook -i production deploy.yml --extra-vars "tag=${GIT_COMMIT_SHORT}" --vault-password-file=vault-password-file
+rm vault-password-file
