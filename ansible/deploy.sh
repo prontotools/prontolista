a77d096f21273d278046d98777e450f05b667e42
zkan <kan@prontomarketing.com>
2019-10-16 08:10:45 +0700

    :rocket: Add SSH key to CircleCI job instead of using actual private key :bear:

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -3,5 +3,5 @@
 export GIT_COMMIT_SHORT=$(echo $CIRCLE_SHA1 | cut -c 1-8)
 echo $VAULT_PASSWORD > vault-password-file
 cp ../api/docker-compose.dev.yml ./roles/deploy/files/
-poetry run ansible-playbook -i development deploy.yml --extra-vars "tag=${GIT_COMMIT_SHORT}" --private-key rocket-prontolista-dev --vault-password-file=vault-password-file
+poetry run ansible-playbook -i development deploy.yml --extra-vars "tag=${GIT_COMMIT_SHORT}" --vault-password-file=vault-password-file
 rm vault-password-file
