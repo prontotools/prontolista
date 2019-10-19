62e58f48d801bafcb80c5bad8f6bbf7cc701c65c
zkan <kan@prontomarketing.com>
2019-10-19 10:36:47 +0700

    :white_check_mark: Change assertion style :bear:

--- api/prontolista/projects/tests/test_admin.py
+++ api/prontolista/projects/tests/test_admin.py
@@ -7,7 +7,4 @@ from ..models import Project
 
 class ProjectAdminTest(TestCase):
     def test_admin_should_be_registered(self):
-        self.assertIsInstance(
-            admin.site._registry[Project],
-            ProjectAdmin
-        )
+        assert isinstance(admin.site._registry[Project], ProjectAdmin)
