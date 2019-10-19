d16afd34fc957d6283132615494097167d413eb1
zkan <kan@prontomarketing.com>
2019-10-19 16:20:30 +0700

    :bug: Remove wrong argument from migrate command :bear:

--- api/compose/django/docker-entrypoint-dev.sh
+++ api/compose/django/docker-entrypoint-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd prontolista
-poetry run python manage.py migrate 0.0.0.0:8000 --settings=prontolista.settings.dev
+poetry run python manage.py migrate --settings=prontolista.settings.dev
 poetry run python manage.py runserver 0.0.0.0:8000 --settings=prontolista.settings.dev
