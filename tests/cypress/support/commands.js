577e0c15413006f2eaba17250304f955cfb28285
zkan <kan@prontomarketing.com>
2019-10-20 15:49:45 +0700

    :white_check_mark: Add Cypress test to check we can create a project :bear:

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+export const generate_random_string = string_length => {
+  let random_string = ''
+  let random_ascii
+  for (let i = 0; i < string_length; i++) {
+    random_ascii = Math.floor(Math.random() * 25 + 97)
+    random_string += String.fromCharCode(random_ascii)
+  }
+  return random_string
+}
