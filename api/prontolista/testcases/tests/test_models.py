152f77221d2193f2eddff204931e1901e59a8b35
zkan <kan@prontomarketing.com>
2019-10-25 08:22:10 +0700

    :white_check_mark: Replace mommy with baker :bear:

--- api/prontolista/testcases/tests/test_models.py
+++ api/prontolista/testcases/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from model_mommy import mommy
+from model_bakery import baker
 
 from ..models import TestCase as TestCaseModel
 from projects.models import Project
@@ -8,7 +8,7 @@ from projects.models import Project
 
 class TestCaseModelTest(TestCase):
     def test_model_should_have_name(self):
-        project = mommy.make(Project)
+        project = baker.make(Project)
         expected_name = "Breadcrumb : click breadcrumb link"
         actual = TestCaseModel.objects.create(name=expected_name, project=project)
 
@@ -18,7 +18,7 @@ class TestCaseModelTest(TestCase):
 
     def test_model_should_have_foreign_key_to_project(self):
         expected_project_name = "Gravity Form test"
-        project = mommy.make(Project, name=expected_project_name)
+        project = baker.make(Project, name=expected_project_name)
         actual = TestCaseModel.objects.create(name="breadcrumb test", project=project)
 
         assert actual.project.name == expected_project_name
@@ -26,7 +26,7 @@ class TestCaseModelTest(TestCase):
         assert actual.modified
 
     def test_model_should_get_name_as_string_representation(self):
-        project = mommy.make(Project)
+        project = baker.make(Project)
         expected_name = "Breadcrumb : click breadcrumb link"
         actual = TestCaseModel.objects.create(name=expected_name, project=project)
 
