88b3c79724478198cab8a134ae613f54a49f48df
zkan <kan@prontomarketing.com>
2019-10-26 06:54:24 +0700

    :sparkles: Display test run column :bear:

--- api/prontolista/test_instances/admin.py
+++ api/prontolista/test_instances/admin.py
@@ -5,5 +5,5 @@ from .models import TestInstance
 
 @admin.register(TestInstance)
 class TestInstanceAdmin(admin.ModelAdmin):
-    list_display = ("id", "testcase", "assignee", "status")
+    list_display = ("id", "testcase", "testrun", "assignee", "status")
     search_fields = ("assignee",)
