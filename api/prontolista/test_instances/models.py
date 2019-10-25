78d383026e3b3219213880a099b30a8bbc4f27ad
zkan <kan@prontomarketing.com>
2019-10-25 09:58:59 +0700

    :card_file_box: Link test instance to test run :bear:

--- api/prontolista/test_instances/models.py
+++ api/prontolista/test_instances/models.py
@@ -3,10 +3,12 @@ from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
 from testcases.models import TestCase
+from testruns.models import TestRun
 
 
 class TestInstance(TimeStampedModel):
     testcase = models.ForeignKey(TestCase, on_delete=models.CASCADE)
+    testrun = models.ForeignKey(TestRun, on_delete=models.CASCADE)
     assignee = models.CharField(null=True, blank=True, max_length=300)
     STATUSES = (("passed", "Passed"), ("blocked", "Blocked"), ("failed", "Failed"))
     status = models.CharField(null=True, blank=True, max_length=300, choices=STATUSES)
