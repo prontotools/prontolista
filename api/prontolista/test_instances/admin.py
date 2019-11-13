0a2c1e45e9616742766ad2baa84f8866baa0dcd3
zkan <kan@prontomarketing.com>
2019-11-13 16:19:57 +0700

    :sparkles: Register comment to admin + able to search comment by text :bear:

--- api/prontolista/test_instances/admin.py
+++ api/prontolista/test_instances/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import TestInstance
+from .models import Comment, TestInstance
 
 
 @admin.register(TestInstance)
 class TestInstanceAdmin(admin.ModelAdmin):
     list_display = ("testcase", "testrun", "status")
     list_filter = ("assignees", "testrun")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("test_instance", "text")
+    search_fields = ("text",)
